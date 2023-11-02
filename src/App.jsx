8b11708fe80e6f89c0652d21333b6ad85bd7a4fb
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2023-11-02 14:43:20 +0100

    updated contract details and app basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,16 @@ import { ethers } from "ethers"
 import { useEffect, useState } from "react"
 import { contractAddress } from "./contractVars/ContractDetails";
 import { ABI } from "./contractVars/ContractDetails";
-function App() {
-  const [owner, setOwner] = useState();
-  const [error, setError] = useState();
-  const [contract, setContract] = useState();
-  const [loading, setLoading] = useState();
-  const [ signer, setSigner ] = useState(null);
-  const [network, setNetwork] = useState('');
-  const [ text, setText ] = useState("Connect wallet");
-  const [ formData, setFormData ] = useState({
-    value : ""
-  });
-  const [ id, setId] = useState('')
 
-  const handleOnChange = (e) => {
-      const { name, value } = e.target;
-      setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-  }
+function App() {
+  const [manager, setManager] = useState("");
+  const [contract, setContract] = useState("");
+  const [error, setError] = useState(null);
+  const [ signer, setSigner ] = useState(null);
+  const [network, setNetwork] = useState("");
+  const [ text, setText ] = useState("Connect wallet");
+  const [ lotteryId, setLotteryId ] = useState("");
+  const [ numOfParticipants, setNumOfParticipants] = useState('')
 
   const connectWallet = async () => {
     setText("Loading")
@@ -34,7 +27,7 @@ function App() {
       console.log(owner)
       console.log(lotteryId)
       setNetwork(network.name.toUpperCase());
-      setOwner(owner)
+      setManager(owner)
       setSigner(signer.address);
     }else{
       alert("Please install a crypto wallet")
@@ -43,16 +36,21 @@ function App() {
 
   const handleOnSubmit = async (e) => {
     e.preventDefault()
-    const tx = await contract.createLottery(`${formData.value}`);
+    const tx = await contract.createLottery(`${numOfParticipants}`);
     console.log(tx);
-    const lotteryData = await contract.LotteryData();
-    console.log(lotteryData)
   }
 
   const handleOnEntry = async (e) => {
-    e.preventDefault()
-    // const tx = await contract.enterLottery(`${id}`, {value: ethdeposit});
-    // console.log(tx);
+    setError(null)
+    try {
+      e.preventDefault();
+    const tx = await contract.enterLottery(`${lotteryId}`, {value: ethers.parseEther("0.01")});
+    console.log(tx);
+    alert("You successfully entered into the lottery");
+    } catch (error) {
+      setError(error.reason)
+    }
+    
   }
 
   useEffect(() => {
@@ -63,7 +61,7 @@ function App() {
     <div className="h-screen flex flex-col gap-6 items-center justify-center">
       <h1 className="text-xl text-red-600">BlockLot</h1>
       <h2>Lottery Contract: {contractAddress}</h2>
-      <h2>Lottery Manager {owner}</h2>
+      <h2>Lottery Manager: {manager}</h2>
       {
         signer && 
         <h1>
@@ -72,25 +70,30 @@ function App() {
       }
     
       <p>Connected to network: {network}</p>
-
+      {
+        error &&  <p className="text-red-600">{error}</p>
+      }
       {
         !signer &&  <button className="bg-gray-300 px-4 py-1 rounded-lg" onClick={connectWallet}>{text}</button>
       }
 
       {
-        signer && 
+        signer === manager ? 
         <form onSubmit={handleOnSubmit} className="flex flex-col gap-5">
-          <input className="border-2 border-black py-1 px-3 w-[400px] rounded" type="text" placeholder="enter value" name="value" value={formData.value} onChange={handleOnChange}/>
+          <input className="border-2 border-black py-1 px-3 w-[400px] rounded" type="text" placeholder="Enter number of participants you want" name="value" value={numOfParticipants} onChange={e => setNumOfParticipants(e.target.value)}/>
           <button className="bg-gray-300 px-4 py-1 rounded-lg">Create Lottery</button>
-        </form>
+        </form> : <div></div> 
+        
       }
 
       {
-        signer && 
-        <form onSubmit={handleOnEntry} className="flex flex-col gap-5">
-          <input className="border-2 border-black py-1 px-3 w-[400px] rounded" type="text" placeholder="enter value" name="value" value={id} onChange={(e) => setId(e.target.value)}/>
-          <button className="bg-gray-300 px-4 py-1 rounded-lg">Enter lottery</button>
-        </form>
+        signer === manager ? 
+          <div></div> 
+          : 
+          <form onSubmit={handleOnEntry} className="flex flex-col gap-5">
+            <input className="border-2 border-black py-1 px-3 w-[400px] rounded" type="text" placeholder="enter lottery" name="id" value={lotteryId} onChange={e => setLotteryId(e.target.value)}/>
+            <button className="bg-gray-300 px-4 py-1 rounded-lg">Enter lottery</button>
+          </form>
       }
     </div>
   )
